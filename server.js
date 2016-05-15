49f97827d72dc6766b75d9bc45bb273b9559b92f
Hallstein <hbrotan@gmail.com>
2016-05-15 08:33:04 +0200

    remove access headers

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use('/', express.static(__dirname));
 
 app.get('/:id', function (req, res) {
